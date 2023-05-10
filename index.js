b6b7343eca287306249940e86337ed1e8ea4b26d
Vladimír Urík <gggedrvideos@gmail.com>
2023-05-10 18:00:13 +0200

    Add discord webhook

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const path = require('path');
 const core = require('@actions/core');
 const exec = require('@actions/exec');
 const GhostAdminApi = require('@tryghost/admin-api');
+const {WebhookClient, EmbedBuilder} = require('discord.js');
 
 (async function main() {
     try {
+        const startTime = Date.now();
+
         const url = core.getInput('api-url');
         const api = new GhostAdminApi({
             url,
@@ -32,9 +35,36 @@ const GhostAdminApi = require('@tryghost/admin-api');
 
         // Deploy it to the configured site
         await api.themes.upload({file: zipPath});
+        const endTime = Date.now();
         console.log(`${zipPath} successfully uploaded.`); // eslint-disable-line no-console
+
+        const timeToDeploy = (endTime - startTime) / 1000;
+        const themeName = core.getInput('theme-name') || require(pkgPath).name;
+        await reportSuccessReportState(themeName, timeToDeploy);
     } catch (err) {
         console.error(JSON.stringify(err, null, 2)); // eslint-disable-line no-console
         process.exit(1);
     }
 }());
+
+async function reportSuccessReportState(themeName, timeToDeploy) {
+    const url = core.getInput('webhook-url') || undefined;
+    if (!url) {
+        return;
+    }
+
+    const pageUrl = core.getInput('page-url');
+
+    const webhook = new WebhookClient({url});
+    const embed = new EmbedBuilder()
+        .setTitle('Theme Deployed')
+        .setDescription(`Theme ${themeName} was successfully deployed in ${timeToDeploy} seconds.`)
+        .setColor('#00ff00')
+        .setTimestamp()
+        .setURL(pageUrl);
+
+    await webhook.send({
+        // eslint-disable-next-line max-lines
+        embeds: [embed]
+    });
+}
